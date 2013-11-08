f3476dea8a654fb5757e3a846b3f5953713e1085
Joshua Peek <josh@joshpeek.com>
2013-11-08 22:30:00 +0000

    Move svg height and width into doc

--- test/perf.js
+++ test/perf.js
@@ -82,9 +82,9 @@
 
 
   function graph(root) {
-    var margin = {top: 20, right: 80, bottom: 30, left: 50},
-        width = 400 - margin.left - margin.right,
-        height = 200 - margin.top - margin.bottom;
+    var margin = {top: 10, right: 10, bottom: 20, left: 30},
+        width = root.width.baseVal.value - margin.left - margin.right,
+        height = root.height.baseVal.value - margin.top - margin.bottom;
 
     var x = d3.scale.linear()
         .range([0, width]);
@@ -126,11 +126,25 @@
         .x(function(d) { return x(xValue(d)); })
         .y(function(d) { return y(yValue(d)); });
 
-    var svg = d3.select(root).append('svg')
-        .attr('width', width + margin.left + margin.right)
-        .attr('height', height + margin.top + margin.bottom)
+    var svg = d3.select(root)
       .append('g')
         .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+    svg.append('g')
+        .attr('class', 'x axis')
+        .attr('transform', 'translate(0, ' + height + ')')
+        .call(xAxis);
+
+    svg.append('g')
+        .attr('class', 'y axis')
+        .call(yAxis)
+      .append('text')
+        .attr('transform', 'rotate(-90)')
+        .attr('y', 6)
+        .attr('dy', '.71em')
+        .style('text-anchor', 'end')
+        .text('ms');
+
 
     function redraw(suite) {
       var data = [ [], [] ];
@@ -144,24 +158,10 @@
       }
 
       x.domain(domain(data, xValue));
+      svg.select('.x.axis').call(xAxis);
+
       y.domain(domain(data, yValue));
-
-      svg.select('.x.axis').remove();
-      svg.append('g')
-          .attr('class', 'x axis')
-          .attr('transform', 'translate(0, ' + height + ')')
-          .call(xAxis);
-
-      svg.select('.y.axis').remove();
-      svg.append('g')
-          .attr('class', 'y axis')
-          .call(yAxis)
-        .append('text')
-          .attr('transform', 'rotate(-90)')
-          .attr('y', 6)
-          .attr('dy', '.71em')
-          .style('text-anchor', 'end')
-          .text('ms');
+      svg.select('.y.axis').call(yAxis);
 
       svg.selectAll('.algorithm').remove();
       var algorithm = svg.selectAll('.algorithm')
