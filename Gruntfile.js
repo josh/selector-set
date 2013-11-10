712ca0debfa0947794c58ee223cfabb1adf75d1c
Joshua Peek <josh@joshpeek.com>
2013-11-10 03:55:34 +0000

    Fix perf jshint task

--- Gruntfile.js
+++ Gruntfile.js
@@ -64,7 +64,7 @@ module.exports = function(grunt) {
             'ExamplarSelectorSet': false
           }
         },
-        src: ['perf/*.js']
+        src: ['test/perf.js']
       }
     },
     qunit: {
