aecce7ddef6c8c0c8b83326744bd3b9ff5bb7ee8
Joshua Peek <josh@joshpeek.com>
2013-12-11 00:31:24 +0000

    Seeded random function

--- test/fuzz.js
+++ test/fuzz.js
@@ -1,7 +1,38 @@
 (function() {
   'use strict';
 
-  test('add', function() {
-    ok(true);
+  function seededRandomFn(seed) {
+    function random() {
+      seed++;
+      seed = (seed * 9301 + 49297) % 233280;
+      return seed / 233280;
+    }
+    return random;
+  }
+
+
+  if (!sessionStorage.seed) {
+    sessionStorage.seed = Math.random();
+  }
+  var suiteSeed = parseFloat(sessionStorage.seed);
+  var suiteRand = seededRandomFn(suiteSeed);
+  var testCount = 10;
+
+  function test(testName, callback) {
+    var i, seed, rand;
+    for (i = 0; i < testCount; i++) {
+      seed = suiteRand();
+      rand = seededRandomFn(seed);
+      QUnit.test(testName + ' (' + seed + ')', function() {
+        callback.call(this, rand);
+      });
+    }
+  }
+
+
+  test('add', function(rand) {
+    ok(true, rand());
+    ok(true, rand());
+    ok(true, rand());
   });
 })();
