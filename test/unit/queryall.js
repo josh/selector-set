f2f415e4d48e02c9dfb7a7e907780afe37ab1e74
Joshua Peek <josh@joshpeek.com>
2014-01-09 14:08:26 -0800

    Move queryAll coverage with group selectors

--- test/unit/queryall.js
+++ test/unit/queryall.js
@@ -73,25 +73,64 @@
   test('compound id and class match', function() {
     var set = new SelectorSet();
     set.add('.foo, .bar');
+    set.add('.foo, .foo');
     set.add('#foo, #bar');
     set.add('#foo, .foo');
     set.add('#bar, .bar');
+    set.add('[class=foo], #foo');
+    set.add('.bar, [id=bar]');
 
     var fixture1 = document.getElementById('fixture1');
     var el1 = fixture1.querySelector('#foo');
     var el2 = fixture1.querySelector('.foo');
     var results = set.queryAll(fixture1);
-    equal(results.length, 3);
+    equal(results.length, 5);
+
     equal(results[0].selector, '.foo, .bar');
     equal(results[0].elements.length, 1);
     equal(results[0].elements[0], el2);
-    equal(results[1].selector, '#foo, #bar');
+
+    equal(results[1].selector, '.foo, .foo');
     equal(results[1].elements.length, 1);
-    equal(results[1].elements[0], el1);
-    equal(results[2].selector, '#foo, .foo');
-    equal(results[2].elements.length, 2);
+    equal(results[0].elements[0], el2);
+
+    equal(results[2].selector, '#foo, #bar');
+    equal(results[2].elements.length, 1);
     equal(results[2].elements[0], el1);
-    equal(results[2].elements[1], el2);
+
+    equal(results[3].selector, '#foo, .foo');
+    equal(results[3].elements.length, 2);
+    equal(results[3].elements[0], el1);
+    equal(results[3].elements[1], el2);
+
+    equal(results[4].elements.length, 2);
+    equal(results[4].elements[0], el1);
+    equal(results[4].elements[1], el2);
+  });
+
+  test('compound id and class match same element', function() {
+    var set = new SelectorSet();
+    set.add('foo, #foo, .foo, [foo=bar]');
+    set.add('bar, #bar, .bar, [bar=foo]');
+    set.add('foo, div, #foo, [foo]');
+
+    var fixture2 = document.getElementById('fixture2');
+    var el1 = fixture2.querySelectorAll('[foo]')[0];
+    var el2 = fixture2.querySelectorAll('[foo]')[1];
+    var el3 = fixture2.querySelectorAll('[foo]')[2];
+
+    var results = set.queryAll(fixture2);
+    equal(results.length, 2);
+
+    equal(results[0].selector, 'foo, #foo, .foo, [foo=bar]');
+    equal(results[0].elements.length, 1);
+    equal(results[0].elements[0], el1);
+
+    equal(results[1].selector, 'foo, div, #foo, [foo]');
+    equal(results[1].elements.length, 3);
+    equal(results[1].elements[0], el1);
+    equal(results[1].elements[1], el2);
+    equal(results[1].elements[2], el3);
   });
 
   test('svg', function() {
