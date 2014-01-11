cb2e7760c76c85218c3faf1f3c15059ea17bd1f4
Joshua Peek <josh@joshpeek.com>
2014-01-11 13:00:00 -0800

    Make parseSelectorIndexes internal

--- selector-set.js
+++ selector-set.js
@@ -174,9 +174,10 @@
   // selector - String CSS selector
   //
   // Returns Array of {index, key}.
-  SelectorSet.prototype.selectorIndexes = function(selector) {
-    var allIndexes = this.indexes.slice(0).concat(this.indexes['default']),
-        allIndexesLen = allIndexes.length,
+  function parseSelectorIndexes(allIndexes, selector) {
+    allIndexes = allIndexes.slice(0).concat(allIndexes['default']);
+
+    var allIndexesLen = allIndexes.length,
         i, j, m, dup, rest = selector,
         key, index, indexes = [];
 
@@ -207,7 +208,7 @@
     } while (m);
 
     return indexes;
-  };
+  }
 
   // Internal: Find first item in Array that is a prototype of `proto`.
   //
@@ -261,7 +262,7 @@
       data: data
     };
 
-    selectorIndexes = this.selectorIndexes(selector);
+    selectorIndexes = parseSelectorIndexes(this.indexes, selector);
     for (i = 0; i < selectorIndexes.length; i++) {
       selectorIndex = selectorIndexes[i];
       key = selectorIndex.key;
@@ -305,7 +306,7 @@
     var removedIds = {};
     var removeAll = arguments.length === 1;
 
-    selectorIndexes = this.selectorIndexes(selector);
+    selectorIndexes = parseSelectorIndexes(this.indexes, selector);
     for (i = 0; i < selectorIndexes.length; i++) {
       selectorIndex = selectorIndexes[i];
 
