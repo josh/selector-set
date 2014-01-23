4600762fd71404f1840639af22b23bc0429268ef
Joshua Peek <josh@joshpeek.com>
2014-01-22 18:29:59 -0600

    Test on Safari 6

--- Gruntfile.js
+++ Gruntfile.js
@@ -90,7 +90,8 @@ module.exports = function(grunt) {
             { browserName: 'internet explorer', version: '11', platform: 'Windows 8.1' },
             { browserName: 'internet explorer', version: '10', platform: 'Windows 8' },
             { browserName: 'internet explorer', version: '9', platform: 'Windows 7' },
-            { browserName: 'safari', version: '7', platform: 'OS X 10.9' }
+            { browserName: 'safari', version: '7', platform: 'OS X 10.9' },
+            { browserName: 'safari', version: '6', platform: 'OS X 10.8' }
           ]
         }
       }
